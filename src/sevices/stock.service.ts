02bad02773a4722fd1f776aa8e9e4a206d5dfbc6
ithai5 <46397063+ithai5@users.noreply.github.com>
2022-05-14 15:27:33 +0200

    wip

--- src/sevices/stock.service.ts
+++ src/sevices/stock.service.ts
@@ -5,5 +5,18 @@ import { StockValue } from "../models/dto/stock-value.dto";
 const { Stock } = switchSelectDatabaseService(Databases.MySQL);
 
 export const getStock = async (stockTicker: string): Promise<StockValue> => {
-  return Stock.getStock(stockTicker);
+  const cacheStockValue= await Stock.getStock(stockTicker);
+  const currentUtcTime = new Date(new Date().toUTCString())
+  const oldestAcceptedUpdateStock = new Date(currentUtcTime.setMinutes(currentUtcTime.getMinutes()-5))
+  if (oldestAcceptedUpdateStock < cacheStockValue.lastUpdated!) {
+    console.log("this is good age")
+    return cacheStockValue
+  }
+  // call api
+
+  // update db
+  // recursion
+  console.log("damn you old")
+
+  return cacheStockValue
 };
